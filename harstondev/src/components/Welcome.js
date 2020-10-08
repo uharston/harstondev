0fff317cf6208ea20fb41798c30b3bff00cf811a
uharston <uharston@gmail.com>
2020-10-08 16:37:39 -0500

    Basic Welcome Container

---
+++ harstondev/src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import { Container, Image } from 'react-bootstrap';
+import  backgroundImage  from '../background.jpg'
+
+function Welcome() {
+    return(
+        <Container className='welcome'>
+            <h1>fhdlkfdsfjdskfjdsflkdsjf;lkdsjf</h1>
+            {/* <Image src={backgroundImage} fluid/> */}
+        </Container>
+    )
+}
+
+export default Welcome; 
