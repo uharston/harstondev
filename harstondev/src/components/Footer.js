9557e79abcc679c4d96224fbb548c458b2513f7f
uharston <uharston@gmail.com>
2020-10-09 16:57:08 -0500

    Started Basic Footer...

---
+++ harstondev/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import { Container } from 'react-bootstrap';
+
+function Footer() {
+    return(
+        <Container className='footer text-white bg-dark'>
+            <p>Logo</p>
+            <p>This site was developed 💯% by Uriah Harston</p>
+            <p> Linkden | Github | Twitter</p>
+        </Container>
+    )
+}
+
+export default Footer; 
