39e05970fbdc3582a5b5000f0612f38013a6ff3b
uharston <uharston@gmail.com>
2020-10-09 16:55:03 -0500

    Started Contact Section...

---
+++ harstondev/src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+import { Container } from 'react-bootstrap';
+
+function Contact() {
+    return(
+        <div>
+            <div className='section-title'>
+                <h1>Contact Me</h1>
+            </div>
+            <Container className='contact bg-dark' fluid>
+                <h1>Contact Information...</h1>
+            </Container>
+        </div>
+    )
+}
+
+export default Contact; 
