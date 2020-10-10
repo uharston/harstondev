70d8c143d343f4f72484b62c555cf0535737f972
uharston <uharston@gmail.com>
2020-10-10 15:21:37 -0500

    Add Components

--- harstondev/src/App.js
+++ harstondev/src/App.js
@@ -2,12 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/NavBar'
+import { Container } from 'react-bootstrap'
+import NavBar from './components/NavBar';
+import Welcome from './components/Welcome';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-dark">
      <NavBar/>
+     <Container className='content' fluid>
+      <Welcome />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+     </Container>
     </div>
   );
 }
